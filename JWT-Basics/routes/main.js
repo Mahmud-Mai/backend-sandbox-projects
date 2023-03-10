c735d69975ffac2e585125e694c2b2b095c6b55a
mahmud.mahmud <mahmud.mahmudx00@gmail.com>
2023-03-10 02:36:26 +0100

    JWT Basics: Remove Typo

--- JWT-Basics/routes/main.js
+++ JWT-Basics/routes/main.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const { login, Dashboard, dashboard } = require('../controllers/main')
+const { login, dashboard } = require('../controllers/main')
 
 router.route('/dashboard').get(dashboard)
 router.route('/login').post(login)
