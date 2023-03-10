8dd11bd997efd649f5b519cfde3f41e954cd562a
mahmud.mahmud <mahmud.mahmudx00@gmail.com>
2023-03-10 02:37:59 +0100

    JWT Basics: Add Validation function for username & password

--- JWT-Basics/controllers/main.js
+++ JWT-Basics/controllers/main.js
@@ -1,10 +1,18 @@
+const CustomAPIError = require("../errors/custom-error");
 
-const login = async (req, req) => {
+const login = async (req, res) => {
+    const { username, password } = req.body
+
+    if(!username || password) {
+        throw new CustomAPIError('Provide email and password', 400)
+    }
+    console.log(username, password);
     res.send('Fake Login/Register/Signup Route')
 }
 
 const dashboard = async (req, res) => {
-    res.status(200).json({ msg:`Hello John Doe`, secret: `Here is your authorized data. Your lucky number is ${number}`})
+    luckyNumber = Math.ceil(Math.random()*100)
+    res.status(200).json({ msg:`Hello John Doe`, secret: `Here is your authorized data. Your lucky number is ${luckyNumber}`})
 }
 
 module.exports = {
