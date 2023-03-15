57d60de38ebe824d0256a3afd45cc8352d6f5ba4
mahmud.mahmud <mahmud.mahmudx00@gmail.com>
2023-03-15 09:35:38 +0100

    feat: create jwt-token as a mongoose instance  method

--- jobs-api/models/User.js
+++ jobs-api/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
  const UserSchema = new mongoose.Schema({
     name: {
@@ -29,5 +30,11 @@ const bcrypt = require('bcryptjs')
     this.password = await bcrypt.hash(this.password, salt)
     next();
  })
+
+ UserSchema.method.createJWT  = function(){
+    return jwt.sign({UserId: this._id, name:this.name},process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_LIFETIME
+    })
+ }
 
  module.exports = mongoose.model('User', UserSchema)
