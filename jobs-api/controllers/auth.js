2a28f0813e302539830fd3b2693c5787702b95f0
mahmud.mahmud <mahmud.mahmudx00@gmail.com>
2023-03-16 06:54:52 +0100

    feat: check login credentials and compare passwords

--- jobs-api/controllers/auth.js
+++ jobs-api/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/User")
 const { StatusCodes } = require("http-status-codes")
+const { BadRequestError, UnauthenticatedError } = require("../errors")
 
 const register = async (req, res) => {
     const user = await User.create({...req.body})
@@ -8,7 +9,25 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
-    res.send('login User')
+    const {email, password} = req.body
+    // check for email and pass
+    if(!email || !password) {
+     throw new BadRequestError('Please provide email and password')
+    }
+
+    const user = await User.findOne({email})
+    if(!password) {
+        throw new UnauthenticatedError('wrong password')
+    }
+    const isPasswordCorrect = await user.comparePass(password)
+    if(!user) {
+        throw new UnauthenticatedError('Invalid credentials')
+    }
+
+    // compare passwords
+    const token = user.createJWT()
+    res.status(StatusCodes.OK).json({user: {name: user.naem}, token})
+
 }
 
 module.exports = {
