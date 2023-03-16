6a6a66a389a5cfc80059e76e62f3d835a468f0f6
mahmud.mahmud <mahmud.mahmudx00@gmail.com>
2023-03-16 17:10:44 +0100

    Feat: include authentication middleware in jobsRoute

--- jobs-api/app.js
+++ jobs-api/app.js
@@ -6,6 +6,9 @@ const app = express();
 // connect DB
 const connectDB = require('./db/connect')
 
+// middleware
+const authenticateUser = require('./middleware/authentication')
+
 // routers
 const authRoute = require('./routes/auth')
 const jobsRoute = require('./routes/jobs')
@@ -19,7 +22,7 @@ app.use(express.json());
 
 // routes
 app.use('/api/v1/auth', authRoute)
-app.use('/api/v1/jobs', jobsRoute)
+app.use('/api/v1/jobs', authenticateUser, jobsRoute)
 
 
 app.use(notFoundMiddleware);
